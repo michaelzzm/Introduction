1c1cf5fe2c3fc9be16fcdc222400a5d343b2d476
flyingice <rainforest.vista@gmail.com>
2014-11-23 23:38:07 +0100

    change mail config

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -13,11 +13,11 @@ return array(
 	'EMAIL_CONFIG' => array(
 	'SMTP_HOST'   => 'smtp.gmail.com', //SMTP服务器
 	'SMTP_PORT'   => 465, //SMTP服务器端口
-	'SMTP_USER'   => 'rainforest.vista@gmail.com', //SMTP服务器用户名
-	'SMTP_PASS'   => 'Yyb-198722', //SMTP服务器密码
-	'FROM_EMAIL'  => 'rainforest.vista@gmail.com', //发件人EMAIL
-	'FROM_NAME'   => 'Yibin', //发件人名称
-	'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+	'SMTP_USER'   => 'team@voluncation.com', //SMTP服务器用户名
+	'SMTP_PASS'   => 'team@2014', //SMTP服务器密码
+	'FROM_EMAIL'  => 'info@voluncation.com', //发件人EMAIL
+	'FROM_NAME'   => '旅心Voluncation', //发件人名称
+	'REPLY_EMAIL' => 'team@voluncation.com', //回复EMAIL（留空则为发件人EMAIL）
 	'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
  	),
 );
