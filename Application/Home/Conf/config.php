fc38b421e81a4ce53452e5027e5d1798b6c41426
flyingice <rainforest.vista@gmail.com>
2014-11-23 21:48:25 +0100

    mail conf

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -9,4 +9,15 @@ return array(
 	'db_port'	=> 3306,
 	'db_name'	=> 'introduction',
 	),
+
+	'EMAIL_CONFIG' => array(
+	'SMTP_HOST'   => 'smtp.gmail.com', //SMTP服务器
+	'SMTP_PORT'   => 465, //SMTP服务器端口
+	'SMTP_USER'   => 'rainforest.vista@gmail.com', //SMTP服务器用户名
+	'SMTP_PASS'   => 'Yyb-198722', //SMTP服务器密码
+	'FROM_EMAIL'  => 'rainforest.vista@gmail.com', //发件人EMAIL
+	'FROM_NAME'   => 'Yibin', //发件人名称
+	'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+	'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
+ 	),
 );
